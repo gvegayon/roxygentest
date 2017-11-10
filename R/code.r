ef9f6936a3b79e62519c66d2b0dc91c566fbf04d
George G. Vega Yon <g.vegayon@gmail.com>
2017-11-10 09:32:20 -0800

    Fixing error

--- R/code.r
+++ R/code.r
@@ -12,34 +12,3 @@
 NULL
 
 
-#' Testing `Sexpr` with `roxygen2` (with `@noMd` at the end)
-#'
-#' In this example, a part of the codechunk IS not parsed correctly,
-#' namely, the `Sexpr` bit, as we set the `@noMd` at the end.
-#'
-#' \Sexpr[results=text]{1+1}
-#'
-#'
-#' @name noMd-end
-#' @noMd
-NULL
-
-#' Testing `Sexpr` with `roxygen2` (with `@noMd` at the middle)
-#'
-#' In this example, a part of the codechunk IS not parsed correctly,
-#' namely, the `Sexpr` bit, as we set the `@noMd` in the middle.
-#'
-#' Actually, right after this text, and when calling `roxygenize` I see the warning
-#'
-#' \preformatted{
-#' Warning: @noMd [code.r#34]: has no parameters
-#' }
-#'
-#'
-#' @noMd
-#'
-#' \Sexpr[results=text]{1+1}
-#'
-#'
-#' @name noMd-middle
-NULL
